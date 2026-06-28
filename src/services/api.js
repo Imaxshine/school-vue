ebc0f95f9fdbc59ed5747f6fadd06ade9a895e49
Imaxshine <imaxshine9@gmail.com>
2026-06-28 19:30:07 +0300

    Live API Testing

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost/school',
+  baseURL: 'https://continental-expenditures-smithsonian-trek.trycloudflare.com/school',//'http://localhost/school',
   timeout: 5000,
   headers: {
     Accept: 'application/json',
--- src/services/loginAPI.js
+++ src/services/loginAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export const loginAPI = axios.create({
-  baseURL: 'http://localhost/school',
+  baseURL: 'https://continental-expenditures-smithsonian-trek.trycloudflare.com/school', //'http://localhost/school',
   timeout: 5000,
   headers: {
     Accept: 'application/json',
